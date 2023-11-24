aa9bcc1960603741ba3a783732c36eb23241777e
Fabiola <fa.martinezo@outlook.es>
2023-11-23 23:04:13 -0600

    Find 2 numbers that added sum a given target

--- two-sum.py
+++ two-sum.py
@@ -4,8 +4,9 @@ def two_sum(arr, target):
     for i in range(arr):
         num2 = target - arr[i]
 
-        if num2 in h_map:
+        if num2 in h_map.keys():
             return [i, h_map[num2]]
         
         h_map[num2] = i
     
+    return -1
